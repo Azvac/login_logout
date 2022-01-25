d5c208d691889dd776ffecb00a2e273975277d6d
Azvac <hugodeschamps2001@gmail.com>
2022-01-25 13:52:35 -0500

    Amélioration de la sécurité - Ajout d'un capcha

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         // Récupération des information d'utilisateur
         $user_name = htmlspecialchars( $_POST[ "username" ] );
         $user_password = htmlspecialchars( $_POST[ "password" ] );
+        $capchaToken = htmlspecialchars( $_POST[ "g-recaptcha-response" ] );
 
         // Connection a la base de données
         include_once "database.php";
@@ -14,14 +15,22 @@
 
         // Vérifie si l'utilisateur existe
         if( is_array( $row ) ) {
-            // Vérification du mot de passe hasher
-            if ( password_verify( $user_password, $row[ 'password' ] ) ) {
+        
+            // Vérifier le capcha
+            $result = json_decode( file_get_contents( "https://www.google.com/recaptcha/api/siteverify?secret=6LcVBjgeAAAAAGWhgCm_gS0XFqNd9xdV5OE1QFXN&response=$capchaToken" ) );
 
-                $_SESSION[ "id" ] = $row[ 'id' ];
-                $_SESSION[ "name" ] = $row[ 'name' ];
+            if ($result->success){
+                // Vérification du mot de passe hasher
+                if ( password_verify( $user_password, $row[ 'password' ] ) ) {
 
-            } else {
-                $message = "Nom d'utilisateur ou mot de passe invalide!";
+                    $_SESSION[ "id" ] = $row[ 'id' ];
+                    $_SESSION[ "name" ] = $row[ 'name' ];
+
+                } else {
+                    $message = "Nom d'utilisateur ou mot de passe invalide!";
+                }
+            } else{
+                $message = "Capcha invalide";
             }
         } else {
             $message = "Nom d'utilisateur ou mot de passe invalide!";
@@ -32,9 +41,11 @@
         header( "Location:connected.php" );
     }
 ?>
+
 <html>
     <head>
         <title>Login</title>
+        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     </head>
     <body>
         <form name="frmUser" method="post" action="" align="center">
@@ -44,6 +55,8 @@
             Nom d'utilisateur :<br><input type="text" name="username"><br />
             Mot de passe :<br><input type="password" name="password"><br />
             
+            <br />
+            <div class="g-recaptcha" data-sitekey="6LcVBjgeAAAAAI2CPlX3Tqx6q4LbsBpmsiDCh4X3" align="center"></div>
             <br />
             <input type="submit">
             <input type="reset">
